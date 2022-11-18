54c228b81e0346e3dbd452562c397488048128da
WTU2 <118342287+WTU2@users.noreply.github.com>
2022-11-18 00:45:22 -0800

    Add files via upload

--- contactmanagementsystem.cpp
+++ contactmanagementsystem.cpp
@@ -1,4 +1,4 @@
-/*this program creates contactsand allows the option to display them through the use of a menu.*/
+/*this program creates contacts and allows the option to display them through the use of a menu.*/
 // side project/practice for C++
 // 11/15/22
 // program created by William Tu
@@ -10,14 +10,13 @@
 using namespace std;
 
 /*allows functions to be called after the user-created function, "void menu(vector<string> contacts)"*/
-int main();
-vector<string> newContact(vector<string> contacts);
-void print(vector<string>& contactNames);
+void newContact(vector<string> contacts);
+void print(const vector<string>& contactNames);
 
 void menu(vector<string> contacts) 	// displays menu for users to select
 {
-	vector<string> addingContacts;
-	int userInp;
+	const vector<string> addingContacts{};
+	int userInp{};
 
 	cout << "	MENU SELECTION" << endl;
 	cout << "---------------------------------" << endl;
@@ -31,29 +30,33 @@ void menu(vector<string> contacts) 	// displays menu for users to select
 	cin >> userInp;
 
 	// choices for users
-	if (userInp == 1)
+	do
 	{
-		newContact(addingContacts); // calling function
-	}
-	else if (userInp == 2)
-	{
-		print(contacts); // calling function
+		switch (userInp)
+		{
+		case 1:
+			newContact(addingContacts);
+			break;
+		case 2:
+			print(contacts);
+			break;
+		case 3:
+			exit(0);
+			break;
+		}
+	} while ((userInp == 1) || (userInp == 2) || (userInp == 3));
 
-	}
-	else if (userInp == 3)
-	{
-		exit(0);  /*stops the program and quits the program too*/
-	}
+	cout << "ERROR: Please enter a correct choice\n" << endl;
 }
 
-vector<string> newContact(vector<string> contacts) 	// creates new contacts
+void newContact(vector<string> contacts) 	// creates new contacts
 {
-	int numberContacts;
-	string contactNames;
-	
+	int numberContacts{};
+	string contactNames{};
+	bool backToMenu;
+
 	cout << "\nEnter NUMBER of contacts\n" << "> ";
 	cin >> numberContacts; // replaces current number of contacts
-	/*not sure how to add onto existing list yet*/
 
 	cout << "\nEnter contact name(s):\n";
 	for (int i = 0; i < numberContacts + 1; ++i)
@@ -63,40 +66,44 @@ vector<string> newContact(vector<string> contacts) 	// creates new contacts
 	}
 	cout << "\nContacts created\n" << endl;
 
-	menu(contacts);
-
-	return contacts;
-
+	while (backToMenu = true)
+	{
+		menu(contacts);
+	}
 
 }
 
-void print(vector<string>& contactNames) // prints vector of names
+void print(const vector<string>& contactNames) // prints vector of names
 {
-	unsigned int i;
 	vector<string> contacts;
 	int userChoice;
 
 	cout << "\nContacts:";
-	for (i = 0; i < contactNames.size(); ++i)
+	for (int i = 0; i < contactNames.size(); ++i)
 	{
 		cout << contactNames.at(i) << endl;
 	}
 
 	/*choices to go back to menu or quit after function finishes dispalying names*/
-	cout << "\n1.) Back to menu\n2.) Quit applciation\n" << "> ";
+	cout << "\n1.) Back to menu\n2.) Quit application\n" << "> ";
 	cin >> userChoice;
 
-	if (userChoice == 1)
+	do
 	{
-		menu(contacts);
-	}
-	else if (userChoice == 2)
-	{
-		cout << "Goodbye!";
-		exit(0);
-	}
+		switch (userChoice)
+		{
+		case 1:
+			menu(contactNames);
+			break;
+		case 2:
+			exit(0);
+			break;
+		}
 
 
+	} while ((userChoice == 1) || (userChoice == 2));
+	
+	cout << "ERROR: Please enter a correct option" << endl;
 }
 
 int main()
@@ -104,9 +111,12 @@ int main()
 	vector<string> contacts; // empty vector
 
 	// call the menu to start off program
-	menu(contacts);
+	while (bool menuSelection = true)
+	{
+		menu(contacts);
+
+	}
 
 
 	system("pause>nul"); /*pauses program so the program doesn't close*/
-	return 0;
 }
